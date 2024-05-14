b6ed0d7c1b4270d0a29271a687b5e872dd0b61f5
JuandaMcMg <juan.marin28310@ucaldas.edu.co>
2024-05-14 01:07:33 -0500

    ServiceExecution adjustment

--- src/app/models/serviceexecution.model.ts
+++ src/app/models/serviceexecution.model.ts
@@ -1,5 +1,8 @@
+import { Customer } from "./customer.model";
+
 export class Serviceexecution {
     id?: number;
-    customer_id: number;
-    service_id: number;
+    customer_id?: number;
+    customer?: Customer;
+    service_id?: number;
 }
