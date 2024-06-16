d4b7cda60be274668200f8eeeb01680c6c181c41
JuandaMcMg <juan.marin28310@ucaldas.edu.co>
2024-06-15 19:59:03 -0500

    session guard

---
+++ src/app/guards/session.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SecurityService } from '../services/security.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionGuard implements CanActivate {
+
+  constructor(private securityService: SecurityService, private router: Router) { }
+  
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.securityService.existSession()) {
+      const confirmLogout = window.confirm("Ya hay una sesión activa. ¿Desea cerrar la sesión actual?");
+      if (confirmLogout) {
+        this.securityService.logout();
+        return true;
+      } else {
+        this.router.navigate(["dashboard"]);
+        return false;
+      }
+    }
+    return true;
+  }
+}
